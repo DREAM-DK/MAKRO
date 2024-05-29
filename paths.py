2caf2258ddbd9eb93bc1e1e5493a174369438d3c
EmilHP <ehp@econ.ku.dk>
2024-05-29 16:18:04 +0200

    - paths update

--- paths.py
+++ paths.py
@@ -1,9 +1,6 @@
 # CHANGE AS NEEDED
-R_path = r"C:\Program Files\R\R-4.2.1"
-GAMS_path = r"C:\GAMS\40"
-GAMS_path_y = r"C:\GAMS\40\gams.exe"
-MAKRO_path = r"C:\Users\B200946\Documents\MAKRO_remove_wageindexing\MAKRO-dev" 
-RSCRIPT_path = 'C:/Program Files/R/R-4.2.1/bin/Rscript'
-# DO NOT CHANGE
-R_arg = '--vanilla'
-gamY_path = MAKRO_path + r"\gamY"
+R_path = r"C:\Program Files\R\R-4.3.2"
+GAMS_path = r"C:\GAMS\46"
+GAMS_path_y = r"C:\GAMS\46\gams.exe"
+RSCRIPT_path = 'C:/Program Files/R/R-4.3.2/bin/Rscript'
+
